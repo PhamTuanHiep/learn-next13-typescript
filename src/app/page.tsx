8df04a30c573c3746de38a99d40505328277dbf6
Tuan Hiep <tuanhiep@gmail.com>
2023-11-16 14:34:58 +0700

    fetch data & rewrite header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
+"use client"
 import Link from 'next/link'
 import styles from './page.module.css'
 import style1 from '@/app/styles/app.module.css'
 import style2 from '@/app/styles/hoidanit.module.css'
 import AppTable from './components/app.table'
+import { useEffect } from 'react'
 
 export default function Home() {
+  useEffect(()=>{
+    const fetchData= async()=>{
+      const res = await fetch('http://localhost:8000/blogs')
+      const data= await res.json()
+      console.log(">>> res:",data)
+    }
+    fetchData()
+  },[]) 
   return (
     <main >
      <div>
