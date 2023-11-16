2b68579b654761e438d4732196a5086c579c4ca9
Tuan Hiep <tuanhiep@gmail.com>
2023-11-16 10:52:22 +0700

    intall & setup bootstrap in next

--- src/app/facebook/page.tsx
+++ src/app/facebook/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 import { useRouter } from 'next/navigation'
 import { use } from 'react'
+import { Button } from 'react-bootstrap';
+
 const FaceBook =()=>{
     const router = useRouter()
     const handleBtn=()=>{
@@ -8,6 +10,7 @@ const FaceBook =()=>{
     }
     return (
         <>
+        <Button variant='primary'>Hoi dan IT</Button>
         <div>Wellcom to FaceBook. Nice to meet you !</div>
         <button onClick={handleBtn}>Back Home</button>
         </>
